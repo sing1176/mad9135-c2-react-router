b3e5a3919844f7978beaffd210b67ff1dcab461d
Sandeep Saini <sing1176@algonquinlive.com>
2021-10-11 19:59:40 -0400

    Add bootstrap and fetched users page and built cards

--- src/components/UsersList/UsersList.js
+++ src/components/UsersList/UsersList.js
@@ -1,11 +1,47 @@
-import React from 'react'
-
+import React from 'react';
+import { useState, useEffect } from 'react';
+import { Card, Button, Stack } from 'react-bootstrap';
+import * as Icon from 'react-bootstrap-icons';
+import './UsersList.css'
 const UsersList = () => {
-   return (
-      <div>
-         <h1>This is Users List</h1>
-      </div>
-   )
-}
+	async function fetchUsers() {
+		let url =
+			'https://randomuser.me/api/?results=16&seed=sing1176&nat=au,ca,nz,gb,us';
+		let res = await fetch(url);
+		let data = await res.json();
+		setUsers(data.results);
+	}
 
-export default UsersList
+	const [users, setUsers] = useState([]);
+
+	useEffect(() => {
+		fetchUsers();
+	}, []);
+
+	console.log(users);
+	return (
+		<>
+			{users.map((user) => (
+				<Card style={{ width: '18rem' }} className="mt-5">
+					<Card.Img variant="top" src={user.picture.large} />
+					<Card.Body>
+						<Card.Title>{user.name.first}</Card.Title>
+						<Card.Text>
+							<p>
+								{' '}
+								<Icon.Phone /> {user.cell}
+							</p>
+							<p>
+								{' '}
+								<Icon.EnvelopeFill /> {user.email}
+							</p>
+						</Card.Text>
+						<Button variant="primary">More info</Button>
+					</Card.Body>
+				</Card>
+			))}
+		</>
+	);
+};
+
+export default UsersList;
